ff5a573c9e613f3913bdae34e839177ddd3a9a85
pascalloria <dev.pascalloria@gmail.com>
2023-04-11 07:54:15 +0200

    Ajout d'un dropdow

--- src/component/Header/UserCard/UserCard.js
+++ src/component/Header/UserCard/UserCard.js
@@ -1,18 +1,32 @@
 import { faUserAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Dropdown } from "react-bootstrap";
 
 const UserCard = (props) => {
     return ( 
        <div className="card">
             
             <div className="card-body">
-                <div className="d-flex justify-content-between align-items-center">
+                <div className="d-flex justify-content-between gap-2 align-items-center">
                     <div ><FontAwesomeIcon icon={faUserAlt} size="xl"/></div>  
                     <div className="card-title d-none d-lg-block flex-grow-1 ">
                         <h4>{props.nickname}</h4>
                         <p>{props.pseudo}</p>
                     </div>
+
+                    <div>
+                        <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">                            
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Deconnection</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Option de profil</Dropdown.Item>                            
+                        </Dropdown.Menu>
+                        </Dropdown>
+                    </div> 
                 </div>
+                
             </div>
         </div>
       
